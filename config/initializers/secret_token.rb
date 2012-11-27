d02341ec1149b8efe8c3bda833d923f382bfcea2
Drew Kitch <drew.kitch@gmail.com>
2012-11-26 16:09:08 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialplanner::Application.config.secret_token = 'c6a0ce9afb6919bcf68013a7f4f2348dbc72f304a2aeff03ac5544092cc73964a695004c33e522eb239d92d16ac2be41512ce6a93b031aa1e6a65cdc63d558a3'
