b5706792811f18856c927ab9cd26984b2a55138e
Apprentice <apprentice@dbc21.local>
2012-11-27 18:05:12 -0800

    mike and nima work

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
-    provider "MyString"
-    uid "MyString"
-    name "MyString"
-    oauth_token "MyString"
+    provider "facebook"
+    uid "21345678540032"
+    name "Billy-Bob Honduras?"
+    oauth_token "DASVAERGH#2345647regGDSFsdfgSERTF"
     oauth_expires_at "2012-11-27 16:51:41"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,67 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:user) { build(:user) } 
+
+	context "validations" do
+
+		it "should have a name" do
+			user.name?.should == true
+		end
+
+		it "should have a unique name" do
+			user.name.unique?.should == true
+		end
+
+		it "has an oauth_token" do
+  		user.oauth_token?.should == true
+  	end
+
+  	it "has a unique oauth_token" do
+  		user.oauth_token.unique?.should == true
+  	end
+
+  	it "has a facebook uid" do
+  		user.uid?.should == true
+  	end
+
+  	it "has a unique facebook uid" do
+  		user.uid.unique?.should == true
+  	end
+
+  	it "has a provider of facebook" do
+  		user.uid.unique?.should == true
+  	end
+	end	
+
+  context "create facebook user" do
+  	it "adds a new user object to the database" do
+  		before_save = user.all.length
+  		after_save  = user.all.length + 1
+  		after_save.should == user.all.length + 1
+  	end
+  end
 end
+
+# class User < ActiveRecord::Base
+#   attr_accessible :name, :oauth_expires_at, :oauth_token, :provider, :uid
+
+#   # validates presence
+#   # validates uniqueness
+
+#   def self.from_omniauth(auth)
+# 	  where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+# 	    user.provider = auth.provider
+# 	    user.uid = auth.uid
+# 	    user.name = auth.info.name
+# 	    user.oauth_token = auth.credentials.token
+# 	    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+# 	    user.save!
+# 	  end
+#   end
+
+#   def facebook
+#   	@facebook ||= Koala::Facebook::API.new(oauth_token)
+#   end
+# end
+# 		
