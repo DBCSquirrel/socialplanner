c7b7c4882ef7178d1c8996030c54a95406de8d4c
Apprentice <apprentice@dbc21.local>
2012-11-27 16:52:06 -0800

    create user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    provider "MyString"
+    uid "MyString"
+    name "MyString"
+    oauth_token "MyString"
+    oauth_expires_at "2012-11-27 16:51:41"
+  end
+end
