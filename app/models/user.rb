db746df81bac45d7a9165dd0f76cdcbaa94ac569
Apprentice <apprentice@dbc30.local>
2012-12-04 13:20:37 -0800

    minor tweak to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,7 @@ class User < ActiveRecord::Base
     attending
   end
 
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
